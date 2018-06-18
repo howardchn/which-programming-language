fb2d67c33645015ae131c1f2fb80a4178de37fb9
Howard Chen <howardch@outlook.com>
2018-06-18 11:20:40 +0800

    defaut route work

---
+++ app/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1 class="main-title">Which Programming Language</h1>
+        <img src="/assets/images/starter.png" />
+        <div>
+            <input id="input_user" class="form-control input-name" type="text" placeholder="Your Name Please" v-model="user" />
+            <button class="btn btn-lg btn-block btn-success button-start" :disabled="user == undefined || user == ''">Start
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data: function() {
+        return {
+            user: 'Howard'
+        }
+    },
+    methods: {
+    }
+}
+</script>
--- app/index.js
+++ app/index.js
@@ -1,10 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/style.css';
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
+import App1 from './App1.vue';
+import Home from './Home.vue';
+
+Vue.use(VueRouter);
+
+let router = new VueRouter({routes: [
+    { name: 'home', path: '/', component: Home, default: true }
+]});
+
+router.replace('/');
 
 new Vue({
     el: '#app',
-    components: { App },
-    template: '<App />'
+    components: { App1 },
+    template: '<App1 />',
+    router: router
 });
