bdca56f3d5cb7ebea135fc90667142cdfdbf526c
Howard Chen <howardch@outlook.com>
2018-06-19 22:24:22 +0800

    gh-pages init

--- app/leanutils.js
+++
@@ -1,6 +0,0 @@
-import AV from 'leancloud-storage';
-
-const { Query, User } = AV;
-AV.init('RRfQUBeDJEwCIoCtSAs7rnaU-gzGzoHsz', 'CDKxBcdvakMOmLbD2LpxTYvc');
-
-export default AV.Object.extend('languages');
--- app/question.js
+++
@@ -1,6 +0,0 @@
-export default class Question {
-    constructor(id, name) {
-        this.id = id;
-        this.name = name;
-    }
-}
